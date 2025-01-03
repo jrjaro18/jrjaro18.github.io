4c4548e2ae6ddfa65dd1e15ef200f9ba9f2c95b8
jrjaro18 <rohanjaiswal1500@gmail.com>
2025-01-03 05:30:03 +0530

    update Vite configuration to set base path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // This allows access from other devices
-    port: 3000, // You can specify any port you want
-  },
+  base: 'steeeve'
 });
