8e234e3da9e13fe65fa5e2ad41cbba62764c7d35
jrjaro18 <rohanjaiswal1500@gmail.com>
2025-01-04 19:10:28 +0530

    testing breaking changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: 'public/404.html', dest: '' }],
-    }),
-  ],
-  base: '/',
+  plugins: [react()],
+  base: '/', // Replace with your repository name or `/` if at the root
 });
