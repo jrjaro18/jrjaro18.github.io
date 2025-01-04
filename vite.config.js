cddf45e7e7f53a56f70697bebe26d7792252dafc
jrjaro18 <rohanjaiswal1500@gmail.com>
2025-01-04 18:55:44 +0530

    handling react routers 404 issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'public/404.html', dest: '' }],
+    }),
+  ],
   base: '/',
-  // server: {
-  //   host: true
-  // }
 });
