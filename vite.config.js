a97328b329c0476ce749b9a6d75847eea2f3f6c6
jrjaro18 <rohanjaiswal1500@gmail.com>
2025-01-04 19:21:14 +0530

    added versioning and reverted breaking changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Replace with your repository name or `/` if at the root
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'public/404.html', dest: '' }],
+    }),
+  ],
+  base: '/',
 });
