0a5114ea5503b60d5bb1a989d883dab2cf7ae492
jrjaro18 <rohanjaiswal1500@gmail.com>
2025-01-10 02:33:13 +0530

    Revert "update background colors for SkeletonLoader in dark mode for better visibility"
    This reverts commit 78439ee8717019f5fbd1bba2612b6bb2708f3c48.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
                 <span className='text-xl text-left md:text-lg'>
                     I primarily build with
                 </span>
-                <div className='cursor-none md:mt-3 mt-4 w-full flex flex-wrap gap-4 md:justify-normal justify-around items-center text-slate-50'>
+                <div className='cursor-none md:mt-3 mt-4 w-full flex flex-wrap gap-4 md:justify-normal justify-evenly items-center text-slate-50'>
                     {
                         techstack.map((tech, index) => (
                             <span key={index} className='bg-dark-blue md:min-w-36 min-w-[45%] shadow-2xl hover:shadow-darker-blue text-center p-2 duration-200 hover:-translate-y-1 hover:bg-darker-blue'>{tech}</span>
