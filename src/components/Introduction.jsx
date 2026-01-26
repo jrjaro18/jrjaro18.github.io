d753efcd01432002bd7b065100a34dcfafdd9e23
jrjaro18 <rohanjaiswal1500@gmail.com>
2026-01-27 02:43:31 +0530

    update for idfc

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -7,36 +7,34 @@ const Introduction = () => {
     const navigate = useNavigate()
     return (
         <>
-            <div className='text-xl text-left md:text-lg'>Hello! I am Rohan Jaiswal :D</div>
-            <ul className='md:mt-2 mt-3 list-inside tracking-wide list-disc'>
+            <div className='text-2xl text-left sm:text-lg sm:p-0 p-2.5'>Hello! <br className='sm:hidden' />I am Rohan Jaiswal :D</div>
+            <ul className='mt-2 list-inside tracking-wide list-disc sm:text-base text-lg flex flex-col gap-y-4 sm:gap-y-2'>
                 <li>
-                    i am a <span className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue font-light duration-300 font-montserrat tracking-wide cursor-pointer' onClick={() => {
+                    i am a <span className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue font-light duration-300 font-montserrat tracking-wide cursor-pointer ' onClick={() => {
                         setToggle(!toggle)
                     }}>
                         {
-                            toggle ? 'YACS' : 'YET ANOTHER COMPUTER SCIENCE'
+                            toggle ? 'YA-CS' : 'YET ANOTHER COMPUTER SCIENCE'
                         }
-                    </span> '25 grad
+                    </span> grad from '25 batch
                 </li>
-                <li className='mt-1'>
-                    i call myself a full stack developer, <br className='md:hidden' /> <span className='md:ml-0 ml-5' /> more of a backend guy tho :P
+                <li className=''>
+                    i have completed my <br className='sm:hidden' /> <span className='sm:ml-0 ml-5' />engineering from<a href='https://maps.app.goo.gl/sb9LQSaBfD9gRgnPA' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'>SPIT</a>, Mumbai
                 </li>
-                <li className='mt-1'>
-                    i like making mobile apps in react <br className='md:hidden' /> <span className='md:ml-0 ml-5' /> native
+                <li className=''>
+                    its been an year since i joined<a href='https://www.linkedin.com/company/idfcfirstbank' className='text-dark-blue hover:text-darker-blue dark:hover:text-light-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'>IDFC</a>,<br className='sm:hidden' /><span className='sm:ml-0 ml-5' /> including a 6 month internship
                 </li>
-                <li className='mt-1'>
-                    golang is my 2nd language
+                <li className=''>
+                    i call myself a full stack developer, <br className='sm:hidden' /> <span className='sm:ml-0 ml-5' /> more of a backend guy tho :P
                 </li>
-                <li className='mt-1'>
-                    tailwind is my bestfriend
+                <li className=''>
+                    i like making mobile apps in react <br className='sm:hidden' /> <span className='sm:ml-0 ml-5' /> native and tailwind
                 </li>
-                <li className='mt-1'>
+                <li className=''>
                     connect with me on:
-                    <a href='https://linkedin.com/in/jrjaro18' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'> Linkedin </a>
-                    <a href='https://github.com/jrjaro18' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'> Github </a>
-                    <br className='md:hidden' /><span className='md:ml-0 ml-4' />
-                    <a href='https://app.daily.dev/jrjaro18' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat mr-1' target='_blank' rel='noreferrer'>Daily.dev </a>
-                    or drop a
+                    <a href='https://linkedin.com/in/jrjaro18' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'> Linkedin</a>, <br className='sm:hidden' /> <span className='sm:ml-0 ml-5' />
+                    <a href='https://github.com/jrjaro18' className='mr-0.5 text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue text-5 duration-300 ml-1 tracking-wide font-light font-montserrat' target='_blank' rel='noreferrer'> Github </a>
+                     or drop a
                     <a href='mailto:jrjaro18@duck.com' className='text-dark-blue hover:text-darker-blue dark:hover:text-lighter-blue duration-300 text-5 ml-1 tracking-wide font-light font-montserrat mr-1' target='_blank' rel='noreferrer'>mail</a>
                 </li>
             </ul>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Skills = () => {
     return (
         <>
-            <div className='mt-4 select-none'>
-                <span className='text-xl text-left md:text-lg'>
+            <div className='mt-4'>
+                <span className='text-xl text-left sm:text-lg select-none'>
                     I primarily build with
                 </span>
-                <div className='cursor-none md:mt-3 mt-4 w-full flex flex-wrap gap-4 md:justify-normal justify-around items-center text-slate-50'>
+                <div className='cursor-none sm:mt-3 mt-6 w-full flex flex-wrap gap-y-5 sm:justify-normal justify-evenly items-center text-slate-50 sm:text-base text-lg'>
                     {
                         techstack.map((tech, index) => (
-                            <span key={index} className='bg-dark-blue md:min-w-36 min-w-[45%] shadow-2xl hover:shadow-darker-blue text-center p-2 duration-200 hover:-translate-y-1 hover:bg-darker-blue'>{tech}</span>
+                            <span key={index} className='bg-dark-blue sm:mx-2 mx-1 sm:min-w-36 sm:px-0 px-2.5 shadow-2xl hover:shadow-darker-blue text-center p-2 duration-200 hover:-translate-y-1 hover:bg-darker-blue '>{tech}</span>
                         ))
                     }
                 </div>
@@ -22,14 +22,18 @@ const Skills = () => {
 export default Skills
 
 const techstack = [
-    'Golang',
+    'Java',
+    'Springboot',
+    'Kafka',
+    'Aerospike',
+    'Connectors',
+    'Docker',
     'React Native',
-    'React',
     'Tailwind CSS',
-    'Supabase',
-    'Node.js',
+    'React',
+    'Golang',
+    'Python',
     'PostgreSQL',
     'MongoDB',
-    'Docker',
-    'Gemini',
+    'Node.js',
 ]
